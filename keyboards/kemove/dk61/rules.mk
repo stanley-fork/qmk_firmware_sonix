2558ecb2a53da4bb18f4f9681cb222eedd085484
Tan `Stanley` Pham <Stanley00@users.noreply.github.com>
2023-07-11 11:05:06 +0700

    [Keyboard] Add Kemove DK61 Snowfox/Shadow initial support

---
+++ keyboards/kemove/dk61/rules.mk
@@ -0,0 +1,26 @@
+# MCU name
+MCU = SN32F248BF
+
+# Build Options
+#   comment out to disable the options.
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = yes            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+
+WAIT_FOR_USB = no
+CUSTOM_MATRIX = yes
+DIP_SWITCH_ENABLE = yes
+
+# Custom RGB matrix handling
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = SN32F24xB
+
+# project specific files
+SRC = ../../drivers/led/sn32/matrix_sn32f24xx.c
+
